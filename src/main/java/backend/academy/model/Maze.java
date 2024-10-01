31531800e12c3c4361e3ed4a41873f2876abb270
ScarFace163 <rarman321123@gmail.com>
2024-10-01 19:30:17 +0300

    Обновлен класс Maze : добавлены поля и конструктор

--- src/main/java/backend/academy/model/Maze.java
+++ src/main/java/backend/academy/model/Maze.java
@@ -1,4 +1,17 @@
 package backend.academy.model;
 
+import lombok.Getter;
+
+@Getter
 public class Maze {
+    private final int height;
+    private final int width;
+    private final Cell[][] grid;
+
+    public Maze(int height, int width, Cell[][] grid) {
+        this.height = height;
+        this.width = width;
+        this.grid = grid;
+    }
+
 }
