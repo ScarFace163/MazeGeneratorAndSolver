0927beaa29d06a33d466874e2191ce0caf93ac6c
ScarFace163 <rarman321123@gmail.com>
2024-10-02 23:58:30 +0300

    Обновлен класс Maze для храния информации о прохождении лабиринта

--- src/main/java/backend/academy/model/Maze.java
+++ src/main/java/backend/academy/model/Maze.java
@@ -1,17 +1,27 @@
 package backend.academy.model;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 public class Maze {
     private final int height;
     private final int width;
     private final Cell[][] grid;
+    @Setter private int visitedCellsCount;
+    @Setter private int percentageOfVisitedCells;
+    @Setter private int passageCellsCount;
 
-    public Maze(int height, int width, Cell[][] grid) {
+    public Maze(
+        int height,
+        int width,
+        Cell[][] grid
+    ) {
         this.height = height;
         this.width = width;
         this.grid = grid;
+        this.visitedCellsCount = 0;
+        this.percentageOfVisitedCells = 0;
+        this.passageCellsCount = 0;
     }
-
 }
