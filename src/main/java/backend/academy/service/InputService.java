ed5e4e5fe3ea28a523f2d6df6fdbcb6de439f6f7
ScarFace163 <rarman321123@gmail.com>
2024-10-09 17:13:30 +0300

    Output and Input Services updated

---
+++ src/main/java/backend/academy/service/InputService.java
@@ -0,0 +1,57 @@
+package backend.academy.service;
+
+import backend.academy.enums.GeneratorStrategyType;
+import backend.academy.enums.SolverStrategyType;
+import java.util.Random;
+import java.util.Scanner;
+
+public class InputService {
+    Scanner sc;
+    Random random;
+    public GeneratorStrategyType inputGenerateStrategyType(){
+        String input = sc.nextLine();
+        if ("".equals(input)) return GeneratorStrategyType.values()[random.nextInt(GeneratorStrategyType.values().length)];
+        try{
+            int number = Integer.parseInt(input);
+            if (number >=0 && number<GeneratorStrategyType.values().length) return GeneratorStrategyType.values()[number];
+            else {
+                return null;
+            }
+        } catch (Exception e){
+            return null;
+        }
+    }
+
+    public InputService() {
+        sc = new Scanner(System.in);
+        random = new Random();
+    }
+
+    public SolverStrategyType inputSolverStrategyType() {
+        String input = sc.nextLine();
+        if ("".equals(input)) return SolverStrategyType.values()[random.nextInt(SolverStrategyType.values().length)];
+        try{
+            int number = Integer.parseInt(input);
+            if (number >=0 && number<SolverStrategyType.values().length) return SolverStrategyType.values()[number];
+            else {
+                return null;
+            }
+        } catch (Exception e){
+            return null;
+        }
+    }
+
+    public int[] inputMazeSize() {
+        String[] cords = sc.nextLine().split(" ");
+        int[] ans = new int[2];
+        if (cords.length != 2) return null;
+        try{
+            ans[0] = Integer.parseInt(cords[0]);
+            ans[1] = Integer.parseInt(cords[1]);
+            if (ans[0]%2==0 || ans[1]%2==0) return null;
+            return ans;
+        } catch (Exception _) {
+            return null;
+        }
+    }
+}
---
+++ src/main/java/backend/academy/service/OutputService.java
@@ -0,0 +1,59 @@
+package backend.academy.service;
+
+import backend.academy.enums.GeneratorStrategyType;
+import backend.academy.enums.SolverStrategyType;
+import backend.academy.model.Cell;
+import backend.academy.model.Maze;
+
+public class OutputService {
+
+    public void print(String text) {
+        System.out.print(text);
+    }
+
+    public void println(String text) {
+        System.out.println(text);
+    }
+
+    public void printMaze(Maze maze) {
+        Cell[][] grid = maze.grid();
+        for (int y = 0; y < grid.length; y++) {
+            for (int x = 0; x < grid[y].length; x++) {
+                switch (grid[y][x].type()) {
+                    case WALL -> print("██");
+                    case PASSAGE -> print("  ");
+                    case VISITED -> print("••");
+                    case PATH -> print("░░");
+                }
+            }
+            print("\n"); // Запись новой строки
+        }
+        println("Passage cells : " + maze.passageCellsCount());
+        println("Visited cells : " + maze.visitedCellsCount());
+        println("Percentage of visited cells : " + maze.percentageOfVisitedCells());
+    }
+    public void printChooseGeneratorTypeMenu(){
+        println("Select the algorithm by which the maze will be generated. Type nothing if you want the algorithm to be selected randomly");
+        for (GeneratorStrategyType type : GeneratorStrategyType.values()){
+            println(type.ordinal() + " - " + type);
+        }
+    }
+    public void printChooseSolverTypeMenu(){
+        println("Select the algorithm by which the maze will be solved. Type nothing if you want the algorithm to be selected randomly");
+        for (SolverStrategyType type : SolverStrategyType.values()){
+            println(type.ordinal() + " - " + type);
+        }
+    }
+
+    public void printTryAgain(){
+        println("Incorrect. Try again");
+    }
+    public void printSelectedStrategies(GeneratorStrategyType generatorStrategyType, SolverStrategyType solverStrategyType){
+        println("Chosen generator type: "+generatorStrategyType);
+        println("Chosen solver type: "+solverStrategyType);
+    }
+
+    public void printChooseMazeSize() {
+        println("Type the size of Maze in format \"a b\" \nBoth numbers must be odd");
+    }
+}
